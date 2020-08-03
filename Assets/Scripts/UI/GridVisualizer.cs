be57e5a8a6659f082b4431ea0e392f749cb7d52a
Juaner-00 <juan.ramirezp@upb.edu.co>
2020-08-03 00:04:26 -0500

    Visualización Grid

---
+++ Assets/Scripts/UI/GridVisualizer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GridManager))]
+public class GridVisualizer : Editor
+{
+    GridManager gridM;
+
+    private void OnEnable()
+    {
+        gridM = target as GridManager;
+        gridM.Start();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Re create grid"))
+            gridM.ResetGrid();
+    }
+
+    private void OnSceneGUI()
+    {
+        Event guiEvent = Event.current;
+        if (guiEvent.type == EventType.MouseDown && guiEvent.button == 0)
+        {
+            Debug.Log("Mouse left click");
+        }
+
+        Draw();
+
+        if (guiEvent.type == EventType.Layout)
+            HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
+    }
+
+    private void Draw()
+    {
+        for (int x = 0; x < gridM.grid.GetWidth(); x++)
+        {
+            for (int z = 0; z < gridM.grid.GetHeight(); z++)
+            {
+                Handles.color = (gridM.grid.GetState(x, z)) ? gridM.canPlaceColor : gridM.cantPlaceColor;
+                Handles.DrawCube(GUIUtility.GetControlID(FocusType.Passive),
+                                 gridM.grid.GetWorldPosition(x, z) + new Vector3(gridM.grid.GetCellSize(), 0, gridM.grid.GetCellSize()) * 0.5f,
+                                 Quaternion.identity,
+                                 gridM.grid.GetCellSize() * 0.9f);
+            }
+        }
+    }
+
+}
---
+++ Assets/ThirdParty/Grid/GridManager.cs
@@ -0,0 +1,65 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField]
+    int height = 50, width = 50;
+
+    [SerializeField]
+    float cellSize = 1;
+
+    [SerializeField]
+    Vector3 originPosition = new Vector3();
+
+    public Color canPlaceColor = new Color(0, 1, 0, 0.5f), cantPlaceColor = new Color(1, 0, 0, 0.5f);
+
+    [HideInInspector]
+    public Grid grid;
+
+    public void Start()
+    {
+        if (grid == null)
+        {
+            grid = new Grid(width, height, cellSize, originPosition);
+            Debug.Log("Create grid");
+        }
+    }
+
+    public void ResetGrid()
+    {
+        grid = new Grid(width, height, cellSize, originPosition);
+        Debug.Log("New grid");
+    }
+
+    private void Update()
+    {
+        HandleClickToModifyGrid();
+
+        if (Input.GetMouseButtonDown(1))
+            Debug.Log(grid.GetState(UtilsClass.GetMouseWorldPosition()));
+    }
+
+    private void HandleClickToModifyGrid()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetState(UtilsClass.GetMouseWorldPosition(), true);
+        }
+    }
+
+}
