02d46da2db20167695ecf5cab492786169a5c18b
Juaner-00 <juan.ramirezp@upb.edu.co>
2020-08-07 20:26:56 -0500

    Mejora al rebote del escudo

---
+++ Assets/Scripts/Mechanics/Rebote.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rebote : MonoBehaviour
+{
+    [SerializeField]
+    float forceMultY = 1f, forceMultX = 1f, forceMultZ = 1f;
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 vel = rb.velocity;
+            rb.AddForce(new Vector3(vel.x * forceMultX, vel.y * forceMultY, vel.z * forceMultZ), ForceMode.Impulse);
+            Debug.Log(new Vector3(vel.x * forceMultX, vel.y * forceMultY, vel.z * forceMultZ));
+        }
+    }
+}
