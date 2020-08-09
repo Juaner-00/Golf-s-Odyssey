5effd9bf4ca79e2ec3abdc9b0c87ddd43390f3b4
Astrodave73 <Astrodave73@github.com>
2020-08-08 23:06:43 -0500

    Mejoras bellas y hermosas

--- Assets/Scripts/Mechanics/HitHole.cs
+++ Assets/Scripts/Mechanics/HitHole.cs
@@ -1,23 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HitHole : MonoBehaviour
 {
+    [SerializeField]
 
-    public ParticleSystem starCelebration;
+    GameObject obStar;
 
-    private void Start()
-    {
-        starCelebration = GetComponent<ParticleSystem>();
-    }
+    [SerializeField]
+
+    Vector3 offset = new Vector3(0, 0, 0);
+    [SerializeField]
+    float time;
     private void OnTriggerEnter(Collider other)
     {
 
-        
-        if (gameObject.CompareTag("Player"))
+
+        if (other.CompareTag("Player"))
         {
-            starCelebration.Play();
+            Instantiate(obStar, transform.position + offset, Quaternion.identity);
+            Invoke("Reset", time);
         }
+    }
+
+    private void Reset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
     }
 }
