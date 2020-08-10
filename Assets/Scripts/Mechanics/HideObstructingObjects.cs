c89f995a69491ecb6b071a240e7f8a254c9af846
Juaner-00 <juan.ramirezp@upb.edu.co>
2020-08-10 00:58:09 -0500

    Ocultar los objetos entre el jugador y la camara

---
+++ Assets/Scripts/Mechanics/HideObstructingObjects.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideObstructingObjects : MonoBehaviour
+{
+    GameObject player;
+    Collider obstruction;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void LateUpdate()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, player.transform.position - transform.position, out hit, 20f))
+        // if (Physics.Raycast(transform.position, player.transform.position - transform.position, out hit, 4.5f))
+        {
+            if (!hit.collider.CompareTag("Player") && !hit.collider.CompareTag("Hole"))
+            {
+                obstruction = hit.collider;
+                MeshRenderer mesh;
+                if (obstruction.TryGetComponent<MeshRenderer>(out mesh))
+                    mesh.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+            }
+            else
+            {
+                if (obstruction != null)
+                {
+                    MeshRenderer mesh;
+                    if (obstruction.TryGetComponent<MeshRenderer>(out mesh))
+                        obstruction.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                }
+            }
+        }
+    }
+
+}
--- Assets/Scripts/UI/Strike.cs
+++ Assets/Scripts/UI/Strike.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class Strike : MonoBehaviour
 {
     [SerializeField]
-    Text strikeText;
+    TextMeshProUGUI strikeText;
 
     PlayerController playerController;
 
