648d9c5135bcd01ba896a531c05339df0b14921e
Astrodave73 <Astrodave73@github.com>
2020-08-11 16:05:24 -0500

    Nuevos Niveles

---
+++ Assets/salpicadura.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class salpicadura : MonoBehaviour {
+    [SerializeField]
+    private GameObject waterSpill;
+    [SerializeField]
+
+    Vector3 offset = new Vector3 (0, 0, 0);
+    [SerializeField]
+    GameObject ballSpill;
+
+    void OnCollisionEnter (Collision collisionInfo) {
+        if (collisionInfo.collider.CompareTag ("Escudo")) {
+            Instantiate (waterSpill, ballSpill.GetComponent<GameObject>().transform.position + offset, Quaternion.identity);
+        }
+    }
+}
