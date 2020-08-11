b8e9280144d373fcf7542471e557b07be41c049d
Juaner-00 <juan.ramirezp@upb.edu.co>
2020-08-10 22:34:10 -0500

    Camera Shaker
    Dependiendo de la fuerza que se le aplica, se pone más o menos amplitud.
    En la main camera se pueden cambiar los valores

--- Assets/Scripts/Player/InputManager.cs
+++ Assets/Scripts/Player/InputManager.cs
@@ -12,49 +12,60 @@ public class InputManager : MonoBehaviour
 
     public event EventHandler OnShoot;
 
+    PlayerController playerController;
+
+
+    private void Awake()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
+
     void Update()
     {
-        // Mouse
+        if (playerController.isStoped)
+        {
+            // Mouse
 
-        // Setear el vector posIni a la posición del mouse
-        if (Input.GetMouseButtonDown(0))
-        {
-            posIni = Input.mousePosition;
-        }
-        // Setear el vector posFin al mantener el botón del mouse
-        else if (Input.GetMouseButton(0))
-        {
-            posFin = Input.mousePosition;
-            vectorSwipe = CalcularDistancia();
-        }
-        // Activar el evento si se soltó el botón del mouse
-        else if (Input.GetMouseButtonUp(0))
-        {
-            OnShoot?.Invoke(this, EventArgs.Empty);
-            vectorSwipe = Vector3.zero;
-        }
-
-        // Touch
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-
-            // Setear el vector posIni a la posición del touch
-            if (touch.phase == TouchPhase.Began)
+            // Setear el vector posIni a la posición del mouse
+            if (Input.GetMouseButtonDown(0))
             {
-                posIni = touch.position;
+                posIni = Input.mousePosition;
             }
-            // Setear el vector posFin a la posición del touch
-            else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+            // Setear el vector posFin al mantener el botón del mouse
+            else if (Input.GetMouseButton(0))
             {
-                posFin = touch.position;
+                posFin = Input.mousePosition;
                 vectorSwipe = CalcularDistancia();
             }
-            // Activar el evento si soltó el touch
-            else if (touch.phase == TouchPhase.Ended)
+            // Activar el evento si se soltó el botón del mouse
+            else if (Input.GetMouseButtonUp(0))
             {
                 OnShoot?.Invoke(this, EventArgs.Empty);
                 vectorSwipe = Vector3.zero;
+            }
+
+            // Touch
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+
+                // Setear el vector posIni a la posición del touch
+                if (touch.phase == TouchPhase.Began)
+                {
+                    posIni = touch.position;
+                }
+                // Setear el vector posFin a la posición del touch
+                else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+                {
+                    posFin = touch.position;
+                    vectorSwipe = CalcularDistancia();
+                }
+                // Activar el evento si soltó el touch
+                else if (touch.phase == TouchPhase.Ended)
+                {
+                    OnShoot?.Invoke(this, EventArgs.Empty);
+                    vectorSwipe = Vector3.zero;
+                }
             }
         }
     }
---
+++ Assets/Third-Party/Scripts/SimpleCameraShakeInCinemachine.cs
@@ -0,0 +1,75 @@
+﻿/*
+See the Unity Tutorial to know how to use this script: https://youtu.be/O2Pg8e2xwzg
+
+Developed by Lumidi - Alex Zuniga: https://twitter.com/LumidiDev
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.Events;
+
+public class SimpleCameraShakeInCinemachine : MonoBehaviour
+{
+
+    [SerializeField] float ShakeDuration = 0.3f;          // Time the Camera Shake effect will last
+    [SerializeField] float ShakeAmplitude = 1.2f;         // Cinemachine Noise Profile Parameter
+    [SerializeField] float ShakeFrequency = 2.0f;         // Cinemachine Noise Profile Parameter
+    [SerializeField] float forceMultAmplitude = 0.2f;
+    [SerializeField] float forceMultFrequency = 0.05f;
+
+    // Cinemachine Shake
+    [SerializeField] CinemachineVirtualCamera VirtualCamera;
+    private CinemachineBasicMultiChannelPerlin virtualCameraNoise;
+
+    PlayerController playerController;
+
+    private float ShakeElapsedTime = 0f;
+
+    private void Awake()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        // Get Virtual Camera Noise Profile
+        if (VirtualCamera != null)
+        {
+            virtualCameraNoise = VirtualCamera.GetCinemachineComponent<Cinemachine.CinemachineBasicMultiChannelPerlin>();
+            playerController.OnStrike += Shake;
+        }
+    }
+
+    private void Shake(int _)
+    {
+        ShakeElapsedTime = ShakeDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the Cinemachine componet is not set, avoid update
+        if (VirtualCamera != null && virtualCameraNoise != null)
+        {
+            // If Camera Shake effect is still playing
+            if (ShakeElapsedTime > 0)
+            {
+                // Set Cinemachine Camera Noise parameters
+                virtualCameraNoise.m_AmplitudeGain = ShakeAmplitude + playerController.forceMag * forceMultAmplitude;
+                virtualCameraNoise.m_FrequencyGain = ShakeFrequency + playerController.forceMag * forceMultFrequency;
+
+                // Update Shake Timer
+                ShakeElapsedTime -= Time.fixedUnscaledDeltaTime;
+            }
+            else
+            {
+                // If Camera Shake effect is over, reset variables
+                virtualCameraNoise.m_AmplitudeGain = 0f;
+                ShakeElapsedTime = 0f;
+            }
+        }
+    }
+}
