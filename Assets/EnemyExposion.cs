f236e8be6088c03d4dfe23c9adb3a63ccaa139c3
Astrodave73 <Astrodave73@github.com>
2020-08-13 16:31:59 -0500

    Progreso nivel, bola y enemigos

---
+++ Assets/EnemyExposion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyExposion : MonoBehaviour
+{
+    public GameObject explo;
+
+    private void Start()
+    {
+        explo = GetComponent<GameObject>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag( "Player"))
+        {
+            Invoke("explo",0);
+            
+            Destroy(gameObject);
+        }
+    }
+}
