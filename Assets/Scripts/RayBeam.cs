878b8f5846e88cecba7de9b959666d1f692b91c9
Astrodave73 <Astrodave73@github.com>
2020-08-14 13:00:30 -0500

    Niveles

--- Assets/Scripts/RayBeam.cs
+++ Assets/Scripts/RayBeam.cs
@@ -38,14 +38,14 @@ public class RayBeam : MonoBehaviour
                 lineRenderer.SetPosition(lineRenderer.positionCount - 1, hit.point);
                 remainingLength -= Vector3.Distance(ray.origin, hit.point);
                 ray = new Ray(hit.point, Vector3.Reflect(ray.direction, hit.normal));
-                if(hit.collider.tag != "Mirror")
-                {
-                    break;
-                }
-                else
+                if (hit.collider.CompareTag ("Bouncers"))
                 {
                     lineRenderer.positionCount += 1;
                     lineRenderer.SetPosition(lineRenderer.positionCount - 1, ray.origin + ray.direction * remainingLength);
+                }
+                else
+                {
+                    break;
                 }
 
                 if (Input.GetMouseButton(0))
