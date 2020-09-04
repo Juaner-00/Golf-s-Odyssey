dcb20f63d8c5405b4b43611679753c82bb49b98a
Juaner-00 <juan.ramirezp@upb.edu.co>
2020-09-03 20:18:53 -0500

    update

--- Assets/Scripts/UI/RayBeam.cs
+++ Assets/Scripts/UI/RayBeam.cs
@@ -21,6 +21,7 @@ public class RayBeam : MonoBehaviour
         lineRenderer = GetComponent<LineRenderer>();
     }
 
+
     private void Update()
     {
         ray = new Ray(transform.position, transform.forward);
@@ -30,30 +31,37 @@ public class RayBeam : MonoBehaviour
 
         float remainingLength = maxLenght;
 
-        for (int i = 0; i < reflections; i++)
+        // lineRenderer.enabled = (Input.GetMouseButton(0) || Input.touchCount > 0) ? true : false;
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, remainingLength))
         {
-            lineRenderer.enabled = (Input.GetMouseButton(0) || Input.touchCount > 0) ? true : false;
-
-            if (Physics.Raycast(ray.origin, ray.direction, out hit, remainingLength))
+            for (int i = 0; i < reflections; i++)
             {
+                lineRenderer.SetPosition(lineRenderer.positionCount, hit.point);
                 lineRenderer.positionCount += 1;
-                lineRenderer.SetPosition(lineRenderer.positionCount - 1, hit.point);
-                remainingLength -= Vector3.Distance(ray.origin, hit.point);
-                ray = new Ray(hit.point, Vector3.Reflect(ray.direction, hit.normal));
 
-                if (hit.collider.CompareTag("Bouncers"))
+                if (Physics.Raycast(ray.origin, ray.direction, out hit, remainingLength))
                 {
-                    lineRenderer.positionCount += 1;
-                    lineRenderer.SetPosition(lineRenderer.positionCount - 1, ray.origin + ray.direction * remainingLength);
+                    if (hit.collider.CompareTag("Bouncers"))
+                    {
+                        remainingLength -= Vector3.Distance(ray.origin, hit.point);
+                        ray = new Ray(hit.point, Vector3.Reflect(ray.direction, hit.normal));
+                        lineRenderer.SetPosition(lineRenderer.positionCount, ray.origin + ray.direction * remainingLength);
+                        lineRenderer.positionCount += 1;
+                    }
+                    else
+                        break;
                 }
                 else
-                {
                     break;
-                }
+
             }
-            else
-                lineRenderer.SetPosition(lineRenderer.positionCount - 1, ray.origin + ray.direction * maxLenght);
         }
+        else
+        {
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPositions(new Vector3[] { transform.position, ray.origin + ray.direction * maxLenght });
+        }
+
 
     }
 }
