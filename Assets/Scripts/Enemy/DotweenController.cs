5515afb2d755cf2c78898e1f88d4463b9f14fc35
Astrodave73 <Astrodave73@github.com>
2020-09-08 09:43:13 -0500

    Avance niveles

--- Assets/Scripts/Enemy/DotweenController.cs
+++ Assets/Scripts/Enemy/DotweenController.cs
@@ -2,18 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using DG.Tweening.Core;
 
 public class DotweenController : MonoBehaviour
 {
+    [SerializeField]
+    private Vector3 _targetLocation = Vector3.zero;
+   
+
+    [Range(1.0f, 10.0f), SerializeField]
+    private float _moveDuration = 1.0f;
+
+    [SerializeField]
+    private Ease _moveEease = Ease.InOutElastic;
+
+ 
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+       
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        transform.DOMove(_targetLocation, _moveDuration).SetEase(_moveEease).SetLoops(-1, LoopType.Yoyo);
+ 
     }
+   
+
 }
