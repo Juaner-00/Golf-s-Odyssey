8859ca252bfe3ad223a4df32fd1fca402708f20f
Astrodave73 <Astrodave73@github.com>
2020-10-30 00:48:49 -0500

    levels

---
+++ Assets/Clouds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clouds : MonoBehaviour
+{
+   Transform cloud;
+    public float speed;
+
+
+    private void Start()
+    {
+        cloud = GetComponent<Transform>();
+    }
+    private void Update()
+    {
+        cloud.transform.position = new Vector3(cloud.transform.position.x + speed * Time.deltaTime, cloud.transform.position.y, cloud.transform.position.z);    
+    }
+
+}
