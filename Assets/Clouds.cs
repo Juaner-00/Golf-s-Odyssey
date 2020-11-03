8b12a3faaca6344cbc7df929269a513083a5ea07
Astrodave73 <Astrodave73@github.com>
2020-11-03 00:26:18 -0500

    sub

--- Assets/Clouds.cs
+++ Assets/Clouds.cs
@@ -6,9 +6,13 @@ public class Clouds : MonoBehaviour
 {
    
     public float speed;
-    [SerializeField]GameObject initialPos;
-   
-    
+    //[SerializeField]
+    GameObject initialPos;
+
+    private void Awake()
+    {
+        initialPos = GameObject.Find("Initial cloud position");
+    }
 
 
     private void Update()
@@ -22,7 +26,7 @@ public class Clouds : MonoBehaviour
 
         if (other.gameObject.CompareTag("Cloud"))
         {
-            Debug.Log("Nube reiniciada");
+            
             transform.position = new Vector3(initialPos.transform.position.x, transform.position.y, transform.position.z);
         }
     }
